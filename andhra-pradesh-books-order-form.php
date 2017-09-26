fda72e559cd2ec6f948398f8a4c90a9ba8d3024d
Srikanth Palagani <india2sree@gmail.com>
2017-09-26 08:26:13 +0530

    removed script files in bottom

--- andhra-pradesh-books-order-form.php
+++ andhra-pradesh-books-order-form.php
@@ -1519,7 +1519,4 @@ while($data1=mysql_fetch_array($andhraSMSSSIsql)) {
 </html>
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="./supportfiles/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="./supportfiles/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="./supportfiles/ie10-viewport-bug-workaround.js"></script>
+ 
